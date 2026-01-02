5d2f8515022ce3484299c78c85475d3e67710648
Artem Reva <artemreva79@gmail.com>
2026-01-02 10:53:21 +0300

    Add vitest config/coverage report

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,37 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as path from 'path';
 
+const include = ['src/**/*.{ts,tsx}'];
+const exclude = [
+  "**/node_modules/**",
+  "**/dist/**",
+  "**/playwright-report/**",
+  "**/playwrightTests/**",
+  "**/test-results/**",
+  "**/coverage/**",
+  "**/src/setupTests.ts"
+];
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(import.meta.dirname, './src')
+    }
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+    exclude,
+    coverage: {
+      enabled: true,
+      provider: 'v8',
+      reporter: ['text', 'lcov'],
+      reportsDirectory: './coverage',
+      include,
+      exclude,
     }
   }
 })
